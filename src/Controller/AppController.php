96a10418ff64f86e4a9e57c5c169605ac75c8674
Jorge Jardim <jorge@conteudodinamico.com.br>
2015-07-17 04:04:14 -0300

    Refactor

--- src/Controller/AppController.php
+++ src/Controller/AppController.php
@@ -1,6 +1,12 @@
 <?php
-
-namespace AkkaFacebook\Controller;
+/**
+ * JorgeFacebook Facebook
+ *
+ * @author Jorge Jardim
+ * @copyright (c) 2015 jorgejardim.com.br
+ * @license MIT
+ */
+namespace JorgeFacebook\Controller;
 
 use App\Controller\AppController as BaseController;
 
